2b91d317cb0c0893e5661f6c761354a4775d9a98
beppe11 <78246566+beppe11@users.noreply.github.com>
2021-02-06 11:23:31 +0900

    NamedExportImportに修正して完成

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@
 import React, { useEffect, useState } from "react";
 import { ColorfulMessage } from "./components/ColorfulMessage";
 
-const App = () => {
+export const App = () => {
   console.log("Start");
   const [num, setNum] = useState(0);
   const [faceShowFlag, setFaceShowFlag] = useState(false);
@@ -39,4 +39,4 @@ const App = () => {
   );
 };
 
-export default App;
+// export default App;
